9ff7ca3ff3befea105485753d5e048358c9705f6
Josué Guardado <jguardadosv@outlook.com>
2021-05-05 14:12:40 -0600

    cambios

--- ejemplotransaccion/Program.cs
+++ ejemplotransaccion/Program.cs
@@ -13,37 +13,40 @@ namespace ejemplotransaccion
         {
 
             string cadena = "Data Source=.;Initial Catalog=CuentaBancaria;Integrated Security=True";
-
             using (SqlConnection connection = new SqlConnection(cadena))
             {
                 connection.Open();
-
                 // Iniciando una transaccion local.
                 SqlTransaction sqlTran = connection.BeginTransaction();
-
                 // Asignando la trasaccion a un objeto sqlcommand.
                 SqlCommand command = connection.CreateCommand();
                 command.Transaction = sqlTran;
 
                 try
                 {
-                    
                     command.CommandText ="Insert into Cuenta values ('Prueba 11',200)";
                     command.ExecuteNonQuery();
-
                     command.CommandText = "Insert into Cuenta values ('Prueba 22',200)";
                     command.ExecuteNonQuery();
-
                     // hacer Commit
                     sqlTran.Commit();
                     Console.WriteLine("Registro insertados.");
                 }
                 catch (Exception ex)
                 {
+                    Console.WriteLine(ex.Message);
 
+                    try
+                    {
+                        // hacer roll back .
+                        sqlTran.Rollback();
+                    }
+                    catch (Exception exRollback)
+                    {
+                        
+                        Console.WriteLine(exRollback.Message);
+                    }
                 }
-
-
             }
             //libera memoria automaticamente
 
